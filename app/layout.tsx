749abb4fd217c759cd8d43131ab1d7242a8d678d
Lucas Pena <lucaspenatorresdev@gmail.com>
2024-11-05 14:25:51 -0300

    feat: add authentication with clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
 
 const font = Inter({
   subsets: ['latin'],
@@ -18,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={`${font.className} dark antialiased`}>{children}</body>
+      <body className={`${font.className} dark antialiased`}>
+        <ClerkProvider appearance={{ baseTheme: dark }}>{children}</ClerkProvider>
+      </body>
     </html>
   )
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,18 @@
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 import Image from 'next/image'
-import { Button } from '../_components/ui/button'
+import { SignInButton } from '@clerk/nextjs'
 import { LogInIcon } from 'lucide-react'
 
-function LoginPage() {
+import { Button } from '../_components/ui/button'
+
+async function LoginPage() {
+  const { userId } = await auth()
+
+  if (userId) {
+    redirect('/')
+  }
+
   return (
     <div className="grid h-full grid-cols-2">
       <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
@@ -13,10 +23,12 @@ function LoginPage() {
           movimentações, e oferecer insights personalizados, facilitando o controle do seu
           orçamento.
         </p>
-        <Button variant={'outline'}>
-          <LogInIcon className="mr-2" />
-          Fazer login ou criar conta
-        </Button>
+        <SignInButton>
+          <Button variant={'outline'}>
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
 
       <div className="relative h-full w-full">
